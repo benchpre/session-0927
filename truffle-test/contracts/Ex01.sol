de964010d495adc4a1e09fd4d53e9cd261077d38
benchpre <101917728+benchpre@users.noreply.github.com>
2022-09-27 01:30:31 +0900

    Add files via upload

---
+++ truffle-test/contracts/Ex01.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Ex01 {
+  string public greeting;
+  constructor(string memory _greeting) {
+    greeting = _greeting;
+  }
+
+  function setGreeting(string memory _greeting) public{
+    greeting = _greeting;
+  }
+  function say() public view returns(string memory){
+    return greeting;
+  }
+}
---
+++ truffle-test/migrations/1_Ex01.js
@@ -0,0 +1,5 @@
+const ex01 - artifacts.require("Ex01");
+
+module.exports = function(deployer){
+    deployer.deploy(ex01, "test, web");
+};
